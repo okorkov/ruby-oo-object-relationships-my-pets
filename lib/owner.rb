e45a3b74251eac8c02afac159263f7071c87e030
okorkov <alex.okorkov@gmail.com>
2020-11-19 16:08:00 -0800

    commit tree d808f2ad3c7340aaf74ba96926b96fc905dd61d0

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,76 @@
 class Owner
-  # code goes here
+
+  attr_accessor :cats, :dogs
+
+    @@all = []
+
+    def initialize(name)
+      @name = name
+      @species = "human"
+      @@all << self
+      @cats = []
+      @dogs = []
+    end
+
+    def name
+      @name
+    end
+
+    def species
+      @species
+    end
+
+    def say_species
+       "I am a #{@species}."
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.count
+      @@all.length
+    end
+
+    def self.reset_all
+      @@all.clear
+    end
+
+    def buy_cat(name)
+      Cat.new(name, self)
+    end
+
+    def buy_dog(name)
+      Dog.new(name, self)
+    end
+
+    def walk_dogs
+      @dogs.each do |dog|
+        dog.mood = "happy"
+      end
+    end
+
+    def feed_cats
+      @cats.each do |cat|
+        cat.mood = "happy"
+      end
+    end
+
+    def sell_pets
+      @cats.each do |cat| 
+        cat.mood = "nervous" 
+        cat.owner = nil
+      end
+      @dogs.each do |dog| 
+        dog.mood = "nervous" 
+        dog.owner = nil
+      end
+      @dogs = []
+      @cats = []
+    end
+
+    def list_pets
+      "I have #{@dogs.count} dog(s), and #{@cats.count} cat(s)."
+    end
+
 end
